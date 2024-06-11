2b7cf4e0f06e1c7c567a9a07ed2169e09024ec8a
Ramanand Kumar <154696635+RamaScript@users.noreply.github.com>
2024-06-11 11:21:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+let gameseq = [];
+let userseq = [];
+
+let btnColors = ["red", "yellow", "purple", "green"];
+
+let started = false;
+
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress", function () {
+  if (started == false) {
+    console.log("khela shuru ho gya ");
+    started = true;
+    levelup();
+  }
+});
+
+function flash(btn) {
+  btn.classList.add("flash");
+  setTimeout(function () {
+    btn.classList.remove("flash");
+  }, 250);
+}
+
+function levelup() {
+  userseq = [];
+  level++;
+  h2.innerText = `level ${level}`;
+
+  let randomBox = Math.floor(Math.random() * 3);
+  let randomColor = btnColors[randomBox];
+  let randomColorBtn = document.querySelector(`.${randomColor}`);
+  gameseq.push(randomColor);
+  console.log(gameseq);
+  flash(randomColorBtn);
+}
+
+function cheakAns(idx) {
+  if (userseq[idx] === gameseq[idx]) {
+    if (userseq.length == gameseq.length) {
+      setTimeout(levelup, 1000);
+    }
+  } else {
+    h2.innerHTML = `game over!! Your Score was <b>${level}</b><i> press any key to play again.</i>`;
+    document.querySelector("body").style.backgroundColor = "red";
+    setTimeout(function(){
+      document.querySelector("body").style.backgroundColor = "burlywood";
+    },100);
+    setTimeout(function(){
+      document.querySelector("body").style.backgroundColor = "red";
+    },200);
+    setTimeout(function(){
+      document.querySelector("body").style.backgroundColor = "burlywood";
+    },300);
+    reset();
+  }
+}
+
+function btnPress() {
+  if (started == true) {
+    userseq.push(this.classList[2]);
+    flash(this);
+    cheakAns(userseq.length - 1);
+  }
+}
+
+let btns = document.querySelectorAll(".btn");
+for (btn of btns) {
+  btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+  started = false;
+  gameseq = [];
+  userseq = [];
+  level = 0;
+}
